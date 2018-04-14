7d06048c31896e0ba1c5068f48b3d7a61be2b842
Xubiod <pitts.davis3@hotmail.com>
2018-04-13 23:03:43 -0400

    actual first commmit

---
+++ rusty-api/example.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+var rusty = require('./lib');
+var port = 1337;
+
+//http
+
+var server = rusty.http.createServer(function (req, res) {
+    rusty.defaultpage(req, function () {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("test");
+    });
+
+    rusty.page(req, "/hello", function () {
+        rusty.writetext(res,
+            "this is write text\n\n",
+            "hello!"
+        );
+    });
+
+    rusty.page(req, "/raw", function () {
+        rusty.writehtml(res,
+            "this is write text<br><br>",
+            "hello!<br><br>",
+            "<h1>I LOVE YOU DOG</h1>"
+        );
+    });
+
+    rusty.errorpage(req, function () {
+        rusty.writestring(res, "error :<");
+    });
+}).listen(port);
---
+++ rusty-api/lib.js
@@ -0,0 +1,46 @@
+﻿//var http = require('http');
+
+module.exports = {
+    http: require('http'),
+
+    //pages
+    page: function (req, url, callback) {
+        if (req.url == url) { callback(); }
+    },
+
+    defaultpage: function (req, callback) {
+        if (req.url == "/") { callback(); }
+    },
+
+    errorpage: function (req, callback) {
+        callback();
+    },
+
+    //write
+    writestring: function (res, text) {
+        res.end(text);
+    },
+
+    writetext: function (res) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        let _write = "";
+        for (i = 1; i < arguments.length; i++) {
+            _write = _write + arguments[i];
+        }
+
+        res.write(_write);
+        res.end();
+    },
+
+    writehtml: function (res) {
+        let _write = "";
+        for (i = 1; i < arguments.length; i++) {
+            _write = _write + arguments[i];
+        }
+
+        res.write("<html>")
+        res.write(_write);
+        res.write("</html>");
+        res.end();
+    }
+}
